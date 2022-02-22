d95c447fa5357ba47958f50aefdf9cecdc240d5e
0xsuk <iam0xsuk@gmail.com>
2022-02-23 03:25:05 +0900

    electron-builder

--- electron/electron.js
+++ electron/electron.js
@@ -1,12 +1,13 @@
 const { app, BrowserWindow } = require("electron");
 const { ipcMain } = require("electron/main");
 const path = require("path");
+const isDev = require("electron-is-dev"); //is not a devDependencies
 const { loadConfig } = require("./api");
 
 // hot reload
-require("electron-reload")(__dirname, {
-  electron: path.join(__dirname, "..", "node_modules", ".bin", "electron"),
-});
+// require("electron-reload")(__dirname, {
+//   electron: path.join(__dirname, "..", "node_modules", ".bin", "electron"),
+// });
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -17,7 +18,11 @@ const createWindow = () => {
     },
   });
 
-  win.loadURL("http://localhost:3000");
+  win.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../index.html")}` //be careful on relative path
+  );
 };
 
 app.whenReady().then(() => {
--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  useNavigate,
-} from "react-router-dom";
-import { Fragment, useEffect, useState } from "react";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Fragment } from "react";
 
 function App() {
   const navigate = useNavigate();
