232396930623f6f41d3a1fe97db77a96022edf89
0xsuk <ikeuu53@yahoo.co.jp>
2022-03-12 15:34:03 +0900

    useFileBuffer

--- src/lib/useFileBuffer.jsx
+++ src/lib/useFileBuffer.jsx
@@ -1,3 +1,76 @@
-function useFileBuffer() {}
+import { useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+//useCodeMirror depends on useFileBuffer's updateDoc
+//filePath is a only dependency.
+function useFileBuffer(filePath) {
+  const [params] = useSearchParams();
+  const navigate = useNavigate();
+  const fileName = params.get("fileName");
+  const [file, setFile] = useState({
+    name: fileName,
+    doc: "",
+    frontmatter: {},
+  });
+
+  const editDoc = (doc) => {
+    setFile((prev) => ({ ...prev, doc }));
+  };
+  const editFileName = async (name) => {
+    setFile((prev) => ({ ...prev, name }));
+  };
+  const editFrontmatter = async (key, value) => {
+    setFile((prev) => ({
+      ...prev,
+      frontmatter: { ...prev.frontmatter, [key]: value },
+    }));
+  };
+
+  const readFile = async (editorView) => {
+    const res = await window.electronAPI.readFile(filePath);
+    if (res.err) {
+      console.log(res.err);
+      return;
+    }
+    editorView.dispatch({
+      changes: {
+        from: 0,
+        to: editorView.state.doc.length, //calls updateDoc
+        insert: res.content,
+      },
+    });
+
+    setFile((prev) => ({ ...prev, frontmatter: res.frontmatter }));
+  };
+
+  const renameFileAndNavigate = async () => {
+    const { newFilePath, err } = await window.electronAPI.renameFile(
+      filePath,
+      file.name
+    );
+    if (err) {
+      alert(err.message);
+      return;
+    }
+    const to = "?path=" + newFilePath + "&isDir=false&fileName=" + file.name;
+    navigate(to);
+  };
+
+  const saveFile = async () => {
+    const { err } = await window.electronAPI.saveFile(
+      file.doc,
+      file.frontmatter,
+      filePath
+    );
+    if (err) {
+      alert(err.message);
+      return;
+    }
+    alert("Saved!");
+    fileName !== file.name && renameFileAndNavigate();
+  };
+
+  return { file, editDoc, editFileName, editFrontmatter, readFile, saveFile };
+}
 
 export default useFileBuffer;
