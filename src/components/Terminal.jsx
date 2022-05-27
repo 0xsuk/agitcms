9ea390c9f26c5ed2e524e18e70fdffe5339072a4
0xsuk <ikeuu53@yahoo.co.jp>
2022-05-28 00:39:32 +0900

    Terminal Toggle

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Terminal as Xterm } from "xterm";
 import { FitAddon } from "xterm-addon-fit";
 import { WebLinksAddon } from "xterm-addon-web-links";
@@ -8,33 +8,54 @@ import useSiteConfig from "../lib/useSiteConfig";
 function Terminal() {
   const siteConfig = useSiteConfig();
   const [cid, setCid] = useState(null);
-  const fitAddon = new FitAddon();
-  const xterm = new Xterm();
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+
   useEffect(() => {
+    if (!siteConfig.path) return;
     let cid; //!important
-    xterm.loadAddon(fitAddon);
-    xterm.loadAddon(new WebLinksAddon());
-    xterm.open(document.getElementById("terminal"));
-    fitAddon.fit();
-    window.addEventListener("resize", () => {
-      fitAddon.fit();
-    });
-    window.electronAPI.onShellData((_, id, data) => {
-      console.log(id, cid);
-      if (id === cid) {
-        xterm.write(data);
+    const el = document.getElementById("terminal");
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "@" && e.ctrlKey) {
+        setIsVisible((prev) => !prev);
+        if (ref.current !== null) return;
+        const xterm = new Xterm();
+        ref.current = xterm;
+        const fitAddon = new FitAddon();
+        xterm.loadAddon(fitAddon);
+        xterm.loadAddon(new WebLinksAddon());
+        xterm.open(el);
+        fitAddon.fit();
+        window.addEventListener("resize", () => {
+          fitAddon.fit();
+        });
+        window.electronAPI.onShellData((_, id, data) => {
+          console.log(id, cid);
+          if (id === cid) {
+            xterm.write(data);
+          }
+        });
+        window.electronAPI.onShellExit((_, exitCode, signal) => {
+          setIsVisible(false);
+          xterm.dispose();
+          ref.current = null;
+        });
+        xterm.onData((data) => {
+          if (data === "\x1B") {
+            setIsVisible(false);
+          }
+          console.log({ data });
+          window.electronAPI.typeCommand(cid, data);
+        });
+        window.electronAPI.spawnShell(siteConfig.path, undefined).then((id) => {
+          setCid(id);
+          cid = id; //!important
+        });
       }
     });
-    window.electronAPI.onShellExit((_, exitCode, signal) => xterm.dispose());
-    xterm.onData((data) => window.electronAPI.typeCommand(cid, data));
+  }, [siteConfig.path]);
 
-    window.electronAPI.spawnShell(siteConfig.path, undefined).then((id) => {
-      setCid(id);
-      cid = id; //!important
-    });
-  }, []);
-
-  return <div id="terminal"></div>;
+  return <div id="terminal" style={{ display: !isVisible && "none" }}></div>;
 }
 
 export default Terminal;
