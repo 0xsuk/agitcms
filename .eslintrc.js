6f4d9bcd05f03ebbd0526a41535b0d44f04569da
0xsuk <ikeuu53@yahoo.co.jp>
2022-08-20 21:29:30 +0900

    eslint modified

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  plugins: ["prettier"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: "6",
+    emcaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+};
