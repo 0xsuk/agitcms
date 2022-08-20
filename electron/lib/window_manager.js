df9e1b0605dccd8a442a84e4dab3e70a50808106
0xsuk <ikeuu53@yahoo.co.jp>
2022-08-20 15:44:21 +0900

    electron background became transparet

--- electron/lib/window_manager.js
+++ electron/lib/window_manager.js
@@ -23,6 +23,7 @@ const createWindow = () => {
     webPreferences: {
       preload: path.join(__dirname, "..", "preload.js"),
     },
+    transparent: true,
   });
 
   attachTitlebarToWindow(win);
