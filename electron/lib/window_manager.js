37a96373da00ba5b02672a4fb2a67c87323f55b3
0xsuk <ikeuu53@yahoo.co.jp>
2022-08-21 17:23:18 +0900

    undo changes

--- electron/lib/window_manager.js
+++ electron/lib/window_manager.js
@@ -5,72 +5,47 @@ const {
   setupTitlebar,
   attachTitlebarToWindow,
 } = require("custom-electron-titlebar/main");
-const { v4 } = require("uuid");
 
-let win;
+let win = undefined;
 
-const defaultWinOptions = {
-  width: 1200,
-  height: 900,
-  titleBarStyle: "hidden",
-  frame: false,
-  minWidth: 400,
-  minHeight: 400,
-  webPreferences: {
-    preload: path.join(__dirname, "..", "preload.js"),
-  },
-  transparent: true,
-};
-class Win {
-  id;
-  win;
-  options;
-
-  constructor(options) {
-    this.id = v4();
-    const finalOptions = Object.assign(defaultWinOptions, options);
-    this.options = finalOptions;
-    this.win = new BrowserWindow(finalOptions);
-
-    this.setup();
-  }
-
-  async setup() {
-    setupTitlebar();
-    this.attachTitlebarToWindow();
-    await this.loadUrl();
-    this.setWindowOpenHandler();
-  }
-
-  attachTitlebarToWindow() {
-    attachTitlebarToWindow(this.win);
-  }
-
-  loadUrl() {
-    return this.win.loadURL(
-      isDev
-        ? "http://localhost:3000"
-        : `file://${path.join(__dirname, "..", "..", "/index.html")}` //be careful on relative path
-    );
-  }
-
-  setWindowOpenHandler() {
-    this.win.webContents.setWindowOpenHandler(({ url }) => {
-      shell.openExternal(url);
-      return { action: "deny" };
-    });
-  }
-}
+setupTitlebar();
 
 const createWindow = () => {
   //Menu.setApplicationMenu(null);
 
-  win = new Win();
+  win = new BrowserWindow({
+    width: 1200,
+    height: 900,
+    titleBarStyle: "hidden",
+    frame: false,
+    minWidth: 400,
+    minHeight: 400,
+    webPreferences: {
+      preload: path.join(__dirname, "..", "preload.js"),
+    },
+    transparent: true,
+  });
+
+  attachTitlebarToWindow(win);
+
+  win
+    .loadURL(
+      isDev
+        ? "http://localhost:3000"
+        : `file://${path.join(__dirname, "..", "..", "/index.html")}` //be careful on relative path
+    )
+    .then(() => {
+      win.webContents.setWindowOpenHandler(({ url }) => {
+        console.log(url);
+        shell.openExternal(url);
+        return { action: "deny" };
+      });
+    });
 };
 
-const getWindow = () => {};
+const getWindow = () => win;
 
-const getWindowById = () => {};
 module.exports = {
   createWindow,
+  getWindow,
 };
