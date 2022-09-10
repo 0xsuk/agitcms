c33c890a9a17d67f7e6a7b05ca81bf494e74d649
0xsuk <97814789+0xsuk@users.noreply.github.com>
2022-09-10 18:58:56 +0900

    refactored

--- src/lib/plugin.js
+++ src/lib/plugin.js
@@ -38,80 +38,56 @@ export class TransactionFilter extends Plugin {
   update(tr) {
     if (!tr.docChanged) return tr;
     const inserted = tr.changes.inserted;
+    function createTr(newChars, from) {
+      return tr.startState.update({
+        changes: {
+          from,
+          insert: newChars,
+        },
+        selection: {
+          anchor: tr.startState.selection.ranges[0].from + newChars.length,
+        },
+        filter: false, //disable TransactionFilter on this update
+      });
+    }
 
-    if (inserted.length === 2 && inserted[1].text.length) {
+    const isSingleChar = inserted.length === 2 && inserted[1].text.length;
+    if (isSingleChar) {
       const oldChar = inserted[1].text[0];
       const newChars = this.map.get(oldChar);
       if (newChars !== undefined) {
-        tr = tr.startState.update({
-          changes: {
-            from: tr.startState.selection.ranges[0].from,
-            insert: newChars,
-          },
-          selection: {
-            anchor: tr.startState.selection.ranges[0].from + newChars.length,
-          },
-          filter: false,
-        });
+        tr = createTr(newChars, tr.startState.selection.ranges[0].from);
       }
     }
     //don't know why but at position 0 (first character) inserted.length is 1
-    if (inserted.length === 1 && inserted[0].text.length) {
+    const isSingleCharAtFirstPos =
+      inserted.length === 1 && inserted[0].text.length;
+    if (isSingleCharAtFirstPos) {
       const oldChar = inserted[0].text[0];
       const newChars = this.map.get(oldChar);
       if (newChars !== undefined) {
-        tr = tr.startState.update({
-          changes: {
-            from: tr.startState.selection.ranges[0].from,
-            insert: newChars,
-          },
-          selection: {
-            anchor: tr.startState.selection.ranges[0].from + newChars.length,
-          },
-          filter: false,
-        });
+        tr = createTr(newChars, tr.startState.selection.ranges[0].from);
       }
     }
 
     //if new line
-    if (
+    const isNewLine =
       this.map.get("\n") !== undefined &&
       inserted.length === 2 &&
       inserted[1].text[0] === "" &&
-      inserted[1].text[1] === ""
-    ) {
-      const newChars = this.map.get("\n");
-      tr = tr.startState.update({
-        changes: {
-          from: tr.startState.selection.ranges[0].from,
-          insert: newChars,
-        },
-        selection: {
-          anchor: tr.startState.selection.ranges[0].from + newChars.length,
-        },
-        filter: false,
-      });
-    }
-
+      inserted[1].text[1] === "";
     //if new line in first position
-    if (
+    const isNewLineAtFirstPos =
       this.map.get("\n") !== undefined &&
       inserted.length === 1 &&
       inserted[0].text[0] === "" &&
-      inserted[0].text[1] === ""
-    ) {
+      inserted[0].text[1] === "";
+    if (isNewLine || isNewLineAtFirstPos) {
       const newChars = this.map.get("\n");
-      tr = tr.startState.update({
-        changes: {
-          from: tr.startState.selection.ranges[0].from,
-          insert: newChars,
-        },
-        selection: {
-          anchor: tr.startState.selection.ranges[0].from + newChars.length,
-        },
-        filter: false,
-      });
+      tr = createTr(newChars, tr.startState.selection.ranges[0].from);
     }
+
+    //TODO: handle tab
 
     if (this.fn) {
       tr = this.fn(tr);
