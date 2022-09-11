58fd33b1ca590d649dab9bfe0d62ae3c0209a2c7
0xsuk <97814789+0xsuk@users.noreply.github.com>
2022-09-11 20:17:35 +0900

    remove default plugins

--- electron/assets/.agitcms/plugins/implicitTool.js
+++
@@ -1,9 +0,0 @@
-new ToolbarItem({
-  initialChar: "", //set no initialChar to represent Implicit tool
-  run: () => {
-    alert(
-      "Implicit tool is not displayed in the toolbar, but can be invoked by keyAlias. Use this if you are sick of clicking item in the toolbar"
-    );
-  },
-  keyAlias: "Ctrl-n",
-});
--- electron/assets/.agitcms/plugins/keymapExample.js
+++
@@ -1,6 +0,0 @@
-new TransactionFilter({
-  map: new Map([
-    ["＃", "#"], //mapping japanese # to english #
-    ["　", " "], //mapping japanese space to english space
-  ]),
-});
