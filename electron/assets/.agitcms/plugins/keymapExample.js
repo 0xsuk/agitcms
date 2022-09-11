d80c886cef34a01d40c40c5338deda0795c93a8f
0xsuk <97814789+0xsuk@users.noreply.github.com>
2022-09-11 20:11:57 +0900

    disable mapping \n by default

--- electron/assets/.agitcms/plugins/keymapExample.js
+++ electron/assets/.agitcms/plugins/keymapExample.js
@@ -2,6 +2,5 @@ new TransactionFilter({
   map: new Map([
     ["＃", "#"], //mapping japanese # to english #
     ["　", " "], //mapping japanese space to english space
-    ["\n", "  \n"], //mapping \n to space+space+\n
   ]),
 });
