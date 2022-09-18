fb2bf6f56119eaf24827f2346784d7b189c7e898
0xsuk <97814789+0xsuk@users.noreply.github.com>
2022-09-18 17:16:26 +0900

    eslint for shared/

---
+++ shared/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  env: {
+    node: true,
+    browser: true,
+  },
+};
