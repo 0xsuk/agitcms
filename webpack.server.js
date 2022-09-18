a3b95816e952e590281405d304351f6d1a5da33d
0xsuk <97814789+0xsuk@users.noreply.github.com>
2022-09-18 20:45:14 +0900

    pass node-pty buld

--- webpack.server.js
+++ webpack.server.js
@@ -1,9 +1,11 @@
 const path = require("path");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   entry: "./server/main.ts",
   //for node, supress polyfill warning
   target: "node",
+  externals: ["node-pty", nodeExternals()],
   output: {
     path: path.join(__dirname, "dist"),
     filename: "main.js",
