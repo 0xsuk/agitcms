a4dbd8f43ddd0a4d7970ea601208e22e7dd18fe2
0xsuk <97814789+0xsuk@users.noreply.github.com>
2022-09-20 17:11:42 +0900

    newSiteDialog with FolderPicker

--- renderer/components/NewSiteDialog.tsx
+++ renderer/components/NewSiteDialog.tsx
@@ -1,4 +1,3 @@
-import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import {
   Button,
   Dialog,
@@ -6,10 +5,53 @@ import {
   DialogContent,
   DialogTitle,
   TextField,
-  Typography,
-  Grid,
 } from "@mui/material";
 import { useState } from "react";
+import FolderPicker from "./FolderPicker";
+
+function Name({ handleSave }: { handleSave: (newName: string) => void }) {
+  const [name, setName] = useState("");
+  const isValid = name !== "";
+
+  return (
+    <>
+      <DialogTitle>Enter a site name</DialogTitle>
+      <DialogContent>
+        <TextField
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          variant="standard"
+          placeholder="Name of the site"
+        ></TextField>
+      </DialogContent>
+      <DialogActions>
+        <Button disabled={!isValid} onClick={() => handleSave(name)}>
+          NEXT
+        </Button>
+      </DialogActions>
+    </>
+  );
+}
+
+function Path({
+  handleSave,
+  handleBack,
+}: {
+  handleSave: (newPath: string) => void;
+  handleBack: () => void;
+}) {
+  return (
+    <>
+      <DialogTitle>Select a site folder</DialogTitle>
+      <DialogContent sx={{ width: "500px", height: "500px" }}>
+        <FolderPicker onPickFolder={(folderPath) => handleSave(folderPath)} />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleBack}>BACK</Button>
+      </DialogActions>
+    </>
+  );
+}
 
 interface Props {
   open: boolean;
@@ -18,69 +60,36 @@ interface Props {
 }
 
 function NewSiteDialog({ open, onClose, addNewSite }: Props) {
-  const [siteName, setSiteName] = useState("");
-  const [sitePath, setSitePath] = useState("");
-  const isValid = siteName !== "" && sitePath !== "";
+  const [name, setName] = useState("");
+  const [path, setPath] = useState("");
 
   const handleClose = () => {
     onClose();
-    setSiteName("");
-    setSitePath("");
+    setName("");
+    setPath("");
   };
 
-  const handleSave = () => {
-    addNewSite(siteName, sitePath);
+  const save = () => {
+    addNewSite(name, path);
     handleClose();
   };
 
-  const editSitePath = async () => {
-    //TODO
-  };
+  if (path !== "") {
+    save();
+    return <></>;
+  }
 
   return (
-    <>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Create New Site</DialogTitle>
-        <DialogContent>
-          <Grid container spacing={2}>
-            <Grid item container spacing={1} alignItems="center">
-              <Grid item>
-                <Typography>Name:</Typography>
-              </Grid>
-              <Grid item>
-                <TextField
-                  value={siteName}
-                  onChange={(e) => setSiteName(e.target.value)}
-                  variant="standard"
-                  placeholder="Name of the site"
-                ></TextField>
-              </Grid>
-            </Grid>
-            <Grid item container spacing={1} alignItems="center">
-              <Grid item>
-                <Typography>Path:</Typography>
-              </Grid>
-              <Grid item>
-                <Typography sx={{ color: "#999" }}>
-                  {sitePath ? sitePath : "select root folder path"}
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Button onClick={editSitePath}>
-                  <DriveFolderUploadOutlinedIcon />
-                </Button>
-              </Grid>
-            </Grid>
-          </Grid>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button disabled={!isValid} onClick={handleSave}>
-            Save
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
+    <Dialog open={open} onClose={handleClose}>
+      {name === "" ? (
+        <Name handleSave={(newName) => setName(newName)} />
+      ) : (
+        <Path
+          handleSave={(newPath) => setPath(newPath)}
+          handleBack={() => setName("")}
+        />
+      )}
+    </Dialog>
   );
 }
 
